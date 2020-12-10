1c678b6ada4b189bae136df747c8f2e0a86eee3b
James Hrivnak <jameshrivnak4@gmail.com>
2020-12-09 19:51:05 -0700

    Complete defining action creators

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,8 +5,20 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
   //TODO:  Write an asynchronous action to handle a video search!
+  //Thunk
+  //Will use dispatch
+  //async calls?
+  //searchYoutube function- inputs: (options object {key, q, max = 5}, callback)
+  //https://codepen.io/stowball/post/a-dummy-s-guide-to-redux-and-thunk-in-react
+  return (dispatch) => {
+    let result = [];
+
+    searchYouTube({key: YOUTUBE_API_KEY, query: q, max: 5}, (items) => result = items);
+    console.log(result);
+    dispatch(changeVideo(result[0]));
+    dispatch(changeVideoList(result));
+  };
 };
 
 export default handleVideoSearch;
